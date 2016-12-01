f8eb36a37a5172be0369605c86b8b9ba8c90c116
Anil Tarte <aniltarte@gmail.com>
2016-12-01 12:46:52 +0530

    started first test

---
+++ src/test/java/cci/BuyOrderJourney.java
@@ -0,0 +1,28 @@
+package cci;
+
+import org.junit.Test;
+
+public class BuyOrderJourney {
+
+    @Test
+    public void itShouldBuyAnOrder(){
+
+        // setup customer
+        // setup advertiser
+        // setup brand
+
+        // setup product
+
+        // setup inventory
+
+        // login
+
+        // get quote
+
+        // draft order
+
+        // add order line
+
+        // buy an order
+    }
+}
